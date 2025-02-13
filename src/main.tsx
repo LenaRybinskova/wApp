7bdfcdf2184369e70070dd56038ad27bf76da3e2
«Elena <LenaRybinskova@gmail.com>
2025-02-13 22:18:00 +0300

    gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import {HashRouter} from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')!).render(
-    <HashRouter basename="/wApp">
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
